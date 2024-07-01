d0b3cad9371c49336b42c83b6595813c6681e018
Alexander Nesterov <alex18nest@icloud.com>
2024-07-01 18:35:12 -0300

    [+] new Account Bank

--- Accounts_dz/main.cpp
+++ Accounts_dz/main.cpp
@@ -6,9 +6,44 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+// Структура для хранения информации о банковском счёте
+struct BankAccount {
+    int accountNumber;
+    string accountHolderName;
+    double balance;
+};
+
+// Функция для изменения баланса счёта
+void changeBalance(BankAccount& account, double newBalance) {
+    account.balance = newBalance;
+}
+
+int main() {
+    BankAccount account;
+
+    cout << "Введите номер счёта: ";
+    cin >> account.accountNumber;
+
+    cout << "Введите имя владельца: ";
+    cin.ignore(); // Удаление символа новой строки из буфера ввода
+    getline(cin, account.accountHolderName); // Ввод имени с пробелами
+
+    cout << "Введите баланс: ";
+    cin >> account.balance;
+
+    double newBalance;
+    cout << "\nВведите новый баланс: ";
+    cin >> newBalance;
+
+    changeBalance(account, newBalance); // Изменяем баланс с помощью функции
+
+    cout << "\nВаш счет: " << account.accountNumber <<
+    ", " << account.accountHolderName <<
+    ", "<< account.balance << endl;
+
     return 0;
 }
