a46eb0e056962760cc3680c7d7d97a5e15cf7a78
Alexander Nesterov <alex18nest@icloud.com>
2024-07-01 17:41:15 -0300

    Initial Commit

---
+++ Accounts_dz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Accounts_dz
+//
+//  Created by Александр Нестеров on 01/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
